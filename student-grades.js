db8b154fcad0305b0d055dd4ba2a90afa2b54b04
Marc Scattolin <marcscattolin@gmail.com>
2022-10-12 11:08:08 -0400

    use bootstrap for table style

--- student-grades.js
+++ student-grades.js
@@ -1,12 +1,14 @@
 var reportSection = document.getElementById("report");
 
 const table = document.createElement("table");
+table.className = "table";
 const tr1 = document.createElement("tr");
 // Append the headings to the first row
 ["Standard Deviation", "Rank", "Percentile"].map(x => {
     const text = document.createTextNode(x);
     const th = document.createElement("th");
     th.appendChild(text);
+    th.scope = "col";
     return th;
 }).forEach(heading => tr1.appendChild(heading));
 // Append the data to the second row
@@ -19,8 +21,12 @@ const element = studentReport.report;
     return td;
 }).forEach(cell => tr2.appendChild(cell));
 
-table.appendChild(tr1);
-table.appendChild(tr2);
+const thead = document.createElement("thead");
+thead.appendChild(tr1);
+const tbody = document.createElement("tbody");
+tbody.appendChild(tr2);
+table.appendChild(thead);
+table.appendChild(tbody);
 
 reportSection.insertAdjacentElement('beforeend', table);
 
