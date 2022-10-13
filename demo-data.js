5fb46e9dd255f968af079d28f8d662f5871739f8
Marc Scattolin <marcscattolin@gmail.com>
2022-10-12 21:00:18 -0400

    make rank and percentile coloured based on the value

--- demo-data.js
+++ demo-data.js
@@ -2,5 +2,5 @@ var studentReport = {
     "assignments": [
     {"name": "Assignment 1", "grade": 80},
     {"name": "Assignment 2", "grade": 75}],
-    report: {"standard-deviation": 14, "rank": 15, "percentile": 89}
+    report: {"standard-deviation": 14, "rank": 20, "percentile": 51}
 };
--- student-grades.js
+++ student-grades.js
@@ -1,4 +1,25 @@
 var reportSection = document.getElementById("report");
+const colourThresholds = {
+    "standard-deviation": null,
+    "rank": new Categorizer([50, 15, 1], ["table-danger", "table-warning", "table-success"], "lowest"),
+    "percentile": new Categorizer([20, 50, 100], ["table-danger", "table-warning", "table-success"], "highest")
+};
+// Constructor of object that will categorize a rank or percentile into the appropriate
+// table colour class to show in the report
+function Categorizer(numbers, classes, best) {
+    this.numbers = numbers;
+    this.classes = classes;
+    this.compare = best == "highest" ? (x,y) => x <= y : (x,y) => y <= x;
+    
+    this.classify = function(num) {
+        for (var i = 0; i < numbers.length; i++) {
+            if (this.compare(num, this.numbers[i])) {
+                return classes[i];
+            }
+        }
+        throw new Error("number " + num + " out of range " + numbers);
+    }
+}
 
 const table = document.createElement("table");
 table.className = "table";
@@ -14,12 +35,22 @@ const tr1 = document.createElement("tr");
 // Append the data to the second row
 const tr2 = document.createElement("tr");
 const element = studentReport.report;
-[element["standard-deviation"], element["rank"], element["percentile"]].map(e => {
-    const text = document.createTextNode(e);
+
+// Create the table element for each item in the report
+for (const reportElement in studentReport.report) {
+    const value = studentReport.report[reportElement]
+    const text = document.createTextNode(value);
     const td = document.createElement("td");
+    // If there is a categorizer for this report element,
+    // categorize the number
+    if (colourThresholds[reportElement]) {
+        const tableClass = colourThresholds[reportElement].classify(value);
+        td.className = tableClass;
+    }
     td.appendChild(text);
-    return td;
-}).forEach(cell => tr2.appendChild(cell));
+    tr2.appendChild(td);
+}
+
 
 const thead = document.createElement("thead");
 thead.appendChild(tr1);
