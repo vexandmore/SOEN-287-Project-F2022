b35ad1901a81ed38d6af7ef030a13db8278a7ebe
Marc Scattolin <marcscattolin@gmail.com>
2022-11-06 13:52:47 -0500

    made it pull the assignment medians from the db

--- api/studentData.php
+++ api/studentData.php
@@ -1,11 +1,39 @@
 <?php
 
+// Connect to database
+$mysqli = new mysqli("127.0.0.1", "root", "", "gms", 3306);
+
+
+// Find all the assignment IDs
+$result = $mysqli->query("SELECT `AssignmentID`,`Name` FROM assignments");
+$assignmentIDs = array();
+foreach ($result as $row) {
+    $assignmentInfo[] = array("id" => $row['AssignmentID'], "name" => $row['Name']);
+}
+
+$assignments = array();
+
+// Find the median of each
+foreach ($assignmentInfo as $info) {
+    $id = $info['id'];
+    $result = $mysqli->query("SELECT Grade FROM grades WHERE AssignmentID=$id ORDER BY Grade ASC");
+    $mid_index = floor($result->num_rows/2);
+    if ($result->num_rows % 2 == 0) {
+        $result->data_seek($mid_index);
+        $mid = $result->fetch_assoc()['Grade'];
+        $result->data_seek($mid_index - 1);
+        $mid_1 = $result->fetch_assoc()['Grade'];
+        $median = ($mid + $mid_1)/2;
+    } else {
+        $result->data_seek($mid_index);
+        $median = $result->fetch_assoc()['Grade'];
+    }
+    $assignments[] = array("name" => $info['name'], "median" => $median);
+}
+
+
 $out = array (
-    "assignments" => array(
-        array("name" => "Assignment1", "median" => 74),
-        array("name" => "Assignment2", "median" => 80),
-        array("name" => "Assignment3", "median" => 90),
-    ),
+    "assignments" => $assignments,
     "studentData" => array(
         array(
             "name" => "Sophie", 
