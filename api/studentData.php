6ab2fe70c3e203795ee7b6b577e679255980699e
Marc Scattolin <marcscattolin@gmail.com>
2022-11-06 14:08:39 -0500

    added comments

--- api/studentData.php
+++ api/studentData.php
@@ -4,7 +4,7 @@
 $mysqli = new mysqli("127.0.0.1", "root", "", "gms", 3306);
 
 
-// Find all the assignment IDs
+// Find all the assignment IDs and names, and putting them in $assignmentInfo
 $result = $mysqli->query("SELECT `AssignmentID`,`Name` FROM assignments");
 $assignmentIDs = array();
 foreach ($result as $row) {
@@ -13,11 +13,15 @@ foreach ($result as $row) {
 
 $assignments = array();
 
-// Find the median of each
+// Find the median of each Assignment
 foreach ($assignmentInfo as $info) {
+    // get the ID for this assignment
     $id = $info['id'];
+    // Get all grades from that ID, in sorted order
     $result = $mysqli->query("SELECT Grade FROM grades WHERE AssignmentID=$id ORDER BY Grade ASC");
+    // Get the index of the middle element in the result
     $mid_index = floor($result->num_rows/2);
+    // If there are an even number of grades
     if ($result->num_rows % 2 == 0) {
         $result->data_seek($mid_index);
         $mid = $result->fetch_assoc()['Grade'];
@@ -25,9 +29,11 @@ foreach ($assignmentInfo as $info) {
         $mid_1 = $result->fetch_assoc()['Grade'];
         $median = ($mid + $mid_1)/2;
     } else {
+        // If there are an odd number of grades
         $result->data_seek($mid_index);
         $median = $result->fetch_assoc()['Grade'];
     }
+    // Add the information about the assignment to the $assignments array
     $assignments[] = array("name" => $info['name'], "median" => $median);
 }
 
