b2ca35843251c0b544caab96eb0e267489a4f647
Marc Scattolin <marcscattolin@gmail.com>
2022-11-08 21:03:53 -0500

    made signup work, but for student only

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'signup';
+$DATABASE_NAME = 'gms';
 
 //connection CHECK
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
@@ -18,7 +18,7 @@ if ( !isset($_POST['firstName'], $_POST['lastName'], $_POST['email']) ) {
 	exit('Please fill both of the name fields and the email field');
 }
 
-if ( empty($_POST['firstName']) || empty($_POST['lastName']) || empty($_POST['email'])) {
+if ( empty($_POST['firstName']) || empty($_POST['lastName']) || empty($_POST['email']) || empty($_POST['studentId'])) {
 	
 	exit('Please complete singup form');
 }
@@ -37,9 +37,9 @@ if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 
 
 // Verifying existence of email (i.e. the account)
-if ($stmt = $con->prepare('SELECT email, password FROM signup WHERE email = ?')) {
+if ($stmt = $con->prepare('SELECT email, `password` FROM students WHERE email = ?')) {
 	
-	$stmt->bind_param('e', $_POST['email']);
+	$stmt->bind_param('s', $_POST['email']);
 	$stmt->execute();
 	$stmt->store_result();
 	// Store the result and check if the email is already registered
@@ -49,8 +49,13 @@ if ($stmt = $con->prepare('SELECT email, password FROM signup WHERE email = ?'))
 	} else {
 	
     //new email, account can be created
-if ($stmt = $con->prepare('INSERT INTO signup (lastName, firstName, email) VALUES (?, ?, ?)')) {
-	
+if ($stmt = $con->prepare('INSERT INTO students (Name, Email, StudentID, Password) VALUES (?, ?, ?, ?)')) {
+	$name = $_POST['firstName'] . ' ' . $_POST['lastName'];
+	$email = $_POST['email'];
+	$id = intval($_POST['studentId']);
+	$password = $_POST['password'];
+	$stmt->bind_param('ssis', $name, $email, $id, $password);
+	$stmt->execute();
 	echo 'You have successfully registered, you can now login!';
 } else {
 	echo 'Could not prepare statement!';
