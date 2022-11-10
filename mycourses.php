7bfbd66c7115027c49ab5571dd5bba558c8b084f
Uniform3 <tristansf@outlook.com>
2022-11-10 15:29:59 -0500

    Changed mycourses.html to mycourses.php  (and added corresponding code)
    There is also a file called studentGrades.php, i commented the code out. I basically just copied the code in studentGrades.php and put it at the top of mycourses

--- mycourses.php
+++ mycourses.php
@@ -1,3 +1,42 @@
+<?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'gms';
+
+// creating a connection and checking whether the connection can be established 
+$connect = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+ if (my_sqli_connect_errno()){
+    exit('Failed to connect to MySQL' .mysqli_connect_error());
+ }
+
+$sql = "SELECT AssignmentID, name, Grade FROM gms";
+$result = $connect->query($sql);
+
+
+// check if the sql database has at least 1 row
+if ($result -> num_rows > 0){
+    // output the value of 
+   echo '<table>
+            <tr>
+                <th> Assignments </th>
+                <th> Grade </th>
+            </tr>'
+    while ($row = $result -> fetch_assoc()){
+        echo '<tr>
+                <td>' "Assignment" .$row["AssignmentID"]. '</td>'
+                '<td>' "Grade: " .$row["Grade"]. '</td>'
+            '</tr>';
+    }
+    echo '</table>'
+
+}
+else{
+    echo "No values to display";
+}
+$connect -> close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
