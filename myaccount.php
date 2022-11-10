101b89f83d260dd10c112dc06cb1d4b451c01b88
Marc Scattolin <marcscattolin@gmail.com>
2022-11-10 12:53:55 -0500

    improve styles for myaccount page, and fix links

--- myaccount.php
+++ myaccount.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
-    echo "ERROR: not logged in";
-    exit;
-}
+
 ?>
 
 
@@ -18,7 +15,15 @@ if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 		
-	
+        <style>
+            table {
+                max-width: 500px;
+            }
+
+            #user-info {
+                padding: 15px;
+            }
+        </style>
     </head>
     <body>
 
@@ -28,8 +33,14 @@ if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
             <div>
                 <ul id="navbar">
 				    <li><a href="home.html">Home</a></li>
-                    <li><a href="mycourses.html">My Courses</a></li>
-					<li><a class="active" href="login.php">My account</a></li>
+                    <?php
+                    if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
+                        echo "<li><a href=\"mycourses.html\">My Courses</a></li>";
+                    } else {
+                        echo "<li><a href=\"teacher page.html\">My Courses</a></li>";
+                    }
+                    ?>
+					<li><a class="active" href="myaccount.php">My account</a></li>
                     <li><a href="logout.php">Logout</a></li>
                 </ul>
             </div>
@@ -40,18 +51,30 @@ if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
         </section>
         <section id="user-info">
             <h3>Data</h3>
-            <table class="table" style="max-width: 500px;">
+            <table class="table">
                 <thead>
                     <th>Name</th>
                     <th>Email</th>
-                    <th>Student ID</th>
+                    <?php
+                    if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
+                        echo "<th>Student ID</th>";
+                    } else {
+                        echo "<th>Teacher ID</th>";
+                    }
+                    ?>
                 </thead>
                 <tbody>
                     <tr>
                         <?php
-                        echo "<td> " . $_SESSION['Name'] . "</td>";
-                        echo "<td> " . $_SESSION['Email'] . "</td>";
-                        echo "<td> " . $_SESSION['StudentID'] . "</td>";
+                        if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)) {
+                            echo "<td> " . $_SESSION['Name'] . "</td>";
+                            echo "<td> " . $_SESSION['Email'] . "</td>";
+                            echo "<td> " . $_SESSION['StudentID'] . "</td>";   
+                        } else {
+                            echo "<td> Luke Lawson </td>";
+                            echo "<td> lukelawson@gmail.com </td>";
+                            echo "<td> 4073615 </td>";
+                        }
                         ?>
                     </tr>
                 </tbody>
