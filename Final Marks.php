e1f7141ab5b4edacb2bfd054b1cdfb56e368950f
mmrida <101475031+mmrida@users.noreply.github.com>
2022-11-16 14:07:44 -0500

    Fixed header and added page footer

--- Final Marks.php
+++ Final Marks.php
@@ -41,7 +41,6 @@ foreach ($result as $row) {
 
     <div>
         <ul id="navbar">
-<li><a href="home.html">Home</a></li>
  <li><a href="mycourses.php">My Courses</a></li>
  <li><a href="myaccount.php">My account</a></li>
  <li><a href="logout.php">Logout</a></li>
@@ -199,6 +198,32 @@ foreach ($result as $row) {
         
        <br>
       </main>
-      <footer class="footer">&copy; Copyright Concordia University.</footer>
     </div>
+      <footer class="section-p1">
+            <div class="col">
+                <i class="bi bi-shop"></i>
+                <h4>Contact</h4>
+                <p><strong>Address: </strong> 640 Callas Street, Los Pierrefonds, Montreal</p>
+                <p><strong>Phone: </strong> +1 (514) 538-4020</p>
+                <div class="follow">
+                    <h4>Follow us</h4>
+                    <div class="icon">
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-instagram"></i>
+                        <i class="bi bi-twitter"></i>
+                        <i class="bi bi-youtube"></i>
+                        <i class="bi bi-pinterest"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="signup page.html">Sign up</a>
+                <a href="mycourses.html">View My Courses</a>
+                <a href="teacher page.html">View Teacher's page</a>
+                <a href="#">User Guide</a>
+            </div>
+        </footer>
   </body>
+
