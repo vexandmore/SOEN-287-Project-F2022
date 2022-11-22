cc0e188778d5e32b0b097c63556c39538fa7fbe3
flysen-berg <114014111+flysen-berg@users.noreply.github.com>
2022-11-22 11:19:05 -0500

    Update average calculator.php

--- average calculator.php
+++ average calculator.php
@@ -1,22 +1,33 @@
 <?php
 
+
+
 if ($_SERVER['REQUEST_METHOD']=='POST')
 
     {
-        #SELECT Grade FROM `grades` WHERE StudentID=40291824;
-        #Dont know how to convert above sql query to php
+    
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'gms';
+
+// creating a connection and checking whether the connection can be established 
+    $connect = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+    $sql = "SELECT AssignmentID, Grade FROM 'grades' WHERE StudentID=40291824" //hardcoded student ID in this part
+    $result = $connect->query($sql);
+    $grades_contents = "";
+
+// snip
+foreach ($result as $row){
+        $grades_contents .= '<tr>';
+        $grades_contents .=    '<td> Assignment ' .$row["AssignmentID"]. '</td>';
+        $grades_contents .=     ' <td> Grade: ' .$row["Grade"]. '</td>';
+        $grades_contents .=    '</tr>';
+    
         
-        $Assignment_1 = floatval ($_POST['Assignment1']);
-        $Assignment_2 = floatval ($_POST['Assignment2']);
-        $Assignment_3 = floatval ($_POST['Assignment3']);
-        $Final = floatval ($_POST['Final']);
-        $Midterm = floatval ($_POST['Midterm']);
-        $Project_1 = floatval ($_POST['Project_1']);
-        $Project_2 = floatval ($_POST['Project_2']);
-        $Q1 = floatval ($_POST['Q1']);
-        $Q2 = floatval ($_POST['Q2']);
-        $Q3 = floatval ($_POST['Q3']);
-        $Marks=(($Q1+$Q2+$Q3+$Project_1+$Project_2+$Assignment_1+$Assignment_2++$Assignment_3+$Midterm+$Final)/9);
+    }
+        $Marks = $results/ //number of assignments? I was not sure what to do in that step
         
         echo $Marks
     }
