6dda45d3b146faad647198b74e7506b5ed444ce1
5DollarCostcoChicken <114677393+5DollarCostcoChicken@users.noreply.github.com>
2022-11-26 13:18:04 -0500

    link styling

--- Final Marks.php
+++ Final Marks.php
@@ -67,10 +67,14 @@ foreach ($result as $row) {
         </ul>
       
       </aside>
-      <h4><li><a class="active" href="teacher page.php">Home</a></li>
-        <li><a href="Assignments.php">Assignments</a>
-        <li><a href="Grades.php">Grades</a></li>
-        <li><a href="Final Marks.php">Final Marks</a></h4>
+      <div>
+        <ul id="navbar">
+        <h1><li><a href="teacher page.php">Home</a></li>
+          <li><a href="Assignments.php">Assignments</a></li>
+          <li><a href="Grades.php">Grades</a></li>
+          <li><a class="active" href="Final Marks.php">Final Marks</a></h1>
+        </ul>
+      </div>
       <main class="content">
         <h2>Final marks:</h2>
         <p>William Lee: A-</p>
