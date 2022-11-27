a39a595f236b781ba4774d570d0ad496936895ab
flysen-berg <114014111+flysen-berg@users.noreply.github.com>
2022-11-27 16:07:46 -0500

    Update average calculator.php

--- average calculator.php
+++ average calculator.php
@@ -1,28 +1,30 @@
 <?php
-
-
-
-if ($_SERVER['REQUEST_METHOD']=='POST')
-
-    {
-    
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'gms';
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'gms';
 
 // creating a connection and checking whether the connection can be established 
-    $connect = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$connect = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
-    $sql = "SELECT AssignmentID, Grade FROM 'grades' WHERE StudentID=40291824" //hardcoded student ID in this part
-    $result = $connect->query($sql);
-    $grades_contents = "";
+$sql = "SELECT AssignmentID, Grade FROM grades WHERE StudentID=40291824"; //hardcoded student ID in this part
+$result = $connect->query($sql);
 
-     if (mysqli_num_rows($result)>0){
-         while($row = mysqli_fetch_assoc($result)){
-             
-           echo "<tr>";
-            echo "<td>" . $row['grades'] . " AVG: " . $row['//final grades? not sure where the average will appear'] . "</td>";
-            echo "</tr>";
-        }
+$grades = [];
+
+foreach ($result as $row) {
+    $grades[] = intval($row['Grade']);
+}
+
+$sum=0;
+$count=0;
+
+foreach ($grades as $grade) {
+    $sum+=$grade;
+    count++;
+}
+
+echo $sum/$count;
+
+
 ?>
