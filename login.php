8acce0cdb71335fa5f5997b965996f75b90cac41
5DollarCostcoChicken <114677393+5DollarCostcoChicken@users.noreply.github.com>
2022-11-28 11:30:37 -0500

    footer changes

--- login.php
+++ login.php
@@ -153,32 +153,7 @@ try {
                     }
         </script>
 	
-	<footer class="section-p1">
-            <div class="col">
-                <i class="bi bi-shop"></i>
-                <h4>Contact</h4>
-                <p><strong>Address: </strong> 640 Callas Street, Los Pierrefonds, Montreal</p>
-                <p><strong>Phone: </strong> +1 (514) 538-4020</p>
-                <div class="follow">
-                    <h4>Follow us</h4>
-                    <div class="icon">
-                        <i class="bi bi-facebook"></i>
-                        <i class="bi bi-instagram"></i>
-                        <i class="bi bi-twitter"></i>
-                        <i class="bi bi-youtube"></i>
-                        <i class="bi bi-pinterest"></i>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <h4>My Account</h4>
-                <a href="signup page.html">Sign up</a>
-                <a href="mycourses.php">View My Courses</a>
-                <a href="teacher page.php">View Teacher's page</a>
-                <a href="#">User Guide</a>
-            </div>
-        </footer>
+	<?php include 'footer.php';?>
 
     </body>
 </html>
